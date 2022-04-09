078d332cbf0d9b3a97c033c5f8508d250015aac3
Darya Zmachinskaya <darya0302@mail.ru>
2022-04-09 21:00:35 +0300

    add search.js

---
+++ src/scripts/search.js
@@ -0,0 +1,17 @@
+let searchInput = document.querySelector('#searchInput');
+searchInput.oninput = function () {
+   let value = this.value.trim();
+   let task = document.querySelectorAll('.card__title');
+
+   if (value != '') {
+      task.forEach(elem => {
+         if (elem.innerText.search(value) == -1) {
+            elem.classList.add('hide');
+         }
+      });
+   } else {
+      task.forEach(elem => {
+         elem.classList.remove('hide');
+      });
+   }
+}
