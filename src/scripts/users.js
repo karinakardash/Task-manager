b8a047f69accd6434400005e390d705d12c16885
KarinaKushnir <karina.kushnir97@gmail.com>
2022-04-15 22:49:55 +0300

    fix bugs for users

--- src/scripts/users.js
+++ src/scripts/users.js
@@ -4,11 +4,11 @@ export async function getUsers() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await response.json();
     console.log(users);
-    const select = displayUsers(users);
 
     const listOfUsers = document.querySelectorAll('.card__user');
     for (let j = 0 ; j < listOfUsers.length; j++) {
+        const select = displayUsers(users);
         console.log(listOfUsers[j]);
-        listOfUsers[j].prepend(select)
+        listOfUsers[j].append(select)
     }
 }
